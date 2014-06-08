c86d35fbfa92d993b01574d28b52b1e0e56aad30
Rforce12 <kalin.g.krastev@gmail.com>
2014-06-08 22:47:46 +0300

    C#

---
+++ ExchageIfGreater.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExchageIfGreater
+{
+    class ExchageIfGreater
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This app exchages values of two integers if the first one is greater");
+            Console.WriteLine();
+            Console.WriteLine("Please enter two numbers (on separate line): ");
+            Console.Write("Enter first number : ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("{0} , {1}", b, a);
+            }
+            else
+            {
+                Console.WriteLine("The first number is not greater than the second one");
+            }
+
+        }
+    }
+}
