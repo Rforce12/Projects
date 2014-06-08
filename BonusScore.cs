26cc8d716f049227471904d3a95d39b13d9a1c4f
Rforce12 <kalin.g.krastev@gmail.com>
2014-06-08 22:44:51 +0300

    C# code

---
+++ BonusScore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BonusScore
+{
+    class BonusScore
+    {
+        static void Main()
+        {
+            Console.WriteLine("This App Checks Bonus Score by Entered Value");
+            Console.WriteLine();
+            Console.Write("Please enter a value (1-9):");
+            int value = int.Parse(Console.ReadLine());
+
+            if (value >= 1 && value <= 9)
+            {
+                if (value >= 1 && value <= 3)
+                {
+                    Console.WriteLine(value * 10);
+
+                }
+                if (value >= 4 && value <= 6)
+                {
+                    Console.WriteLine(value * 100);
+                }
+                if (value >= 7 && value <= 9)
+                {
+                    Console.WriteLine(value * 1000);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("invalid score");
+
+            }
+
+        }
+    }
+}
---
+++ CheckForAPlayCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CheckForAPlayCard
+{
+    class CheckForAPlayCard
+    {
+        static void Main()
+        {
+            Console.Write("Please enter a play card sign to check if it is valid :");
+            string card = Console.ReadLine().ToUpper();
+
+            switch (card)
+            {
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "10":
+                case "Q":
+                case "J":
+                case "K":
+                case "A":
+                    Console.WriteLine("yes");
+                    break;
+
+                default:
+                    Console.WriteLine("no");
+                    break;
+            }
+        }
+    }
+}
